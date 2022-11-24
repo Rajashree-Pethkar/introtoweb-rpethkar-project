03dfc144e57f9af373c96668225edfe1e6c12704
rajashree <rpethkar@pdx.edu>
2022-11-24 11:00:28 -0800

    Added navbar, aboutme, skills and experience

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    var flag;
+    if (flag == undefined) {
+        const typedTextSpan = document.querySelector(".typed-text");
+        const textArray = ["designer", "developer", "programmer"];
+        const typingDelay = 100;
+        const erasingDelay = 100;
+        const newTextDelay = 2000;
+        let textArrayIndex = 0;
+        let charIndex = 0;
+        function type() {
+            if (charIndex < textArray[textArrayIndex].length) {
+                typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+                charIndex++;
+                setTimeout(type, typingDelay);
+            }
+            else {
+                setTimeout(erase, newTextDelay);
+            }
+        }
+        function erase() {
+            if (charIndex > 0) {
+                typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+                charIndex--;
+                setTimeout(erase, erasingDelay);
+            }
+            else {
+                textArrayIndex++;
+                if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+                setTimeout(type, typingDelay);
+            }
+        }
+
+        document.addEventListener("DOMContentLoaded", function () {
+            setTimeout(type, newTextDelay);
+        });
+
+        type();
+    }
+});
